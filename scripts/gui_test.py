efc10f05a4f91e919b429efc64461336874fab58
Chris Crawford <chris35469@gmail.com>
2025-07-02 20:13:41 -0500

    Add pyinstaller

--- scripts/gui_test.py
+++ scripts/gui_test.py
@@ -7,18 +7,34 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.setWindowTitle("Temperature Monitor")
+
         # Temperature vs time dynamic plot
         self.plot_graph = pg.PlotWidget()
-        self.setCentralWidget(self.plot_graph)
+        self.update_freq = 200
+        # self.setCentralWidget(self.plot_graph)
+        # Create a button and add it to a layout
+        self.button = QtWidgets.QPushButton("Start/Stop")
+        self.button.clicked.connect(self.toggle_timer)
+        
+        # Create layout to hold plot and button
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.plot_graph)
+        layout.addWidget(self.button)
+        
+        # Create a widget to hold the layout
+        widget = QtWidgets.QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
         self.plot_graph.setBackground("w")
         pen = pg.mkPen(color=(255, 0, 0))
-        self.plot_graph.setTitle("Temperature vs Time", color="b", size="20pt")
-        styles = {"color": "red", "font-size": "18px"}
-        self.plot_graph.setLabel("left", "Temperature (°C)", **styles)
-        self.plot_graph.setLabel("bottom", "Time (min)", **styles)
-        self.plot_graph.addLegend()
-        self.plot_graph.showGrid(x=True, y=True)
-        self.plot_graph.setYRange(20, 40)
+        # self.plot_graph.setTitle("Temperature vs Time", color="b", size="20pt")
+        # styles = {"color": "red", "font-size": "18px"}
+        # self.plot_graph.setLabel("left", "Temperature (°C)", **styles)
+        # self.plot_graph.setLabel("bottom", "Time (min)", **styles)
+        # self.plot_graph.addLegend()
+        # self.plot_graph.showGrid(x=False, y=True)
+        # self.plot_graph.setYRange(20, 40)
         self.time = list(range(10))
         self.temperature = [randint(20, 40) for _ in range(10)]
         # Get a line reference
@@ -27,15 +43,15 @@ class MainWindow(QtWidgets.QMainWindow):
             self.temperature,
             name="Temperature Sensor",
             pen=pen,
-            symbol="+",
-            symbolSize=15,
-            symbolBrush="b",
         )
         # Add a timer to simulate new temperature measurements
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(300)
+        self.timer.setInterval(self.update_freq)
         self.timer.timeout.connect(self.update_plot)
         self.timer.start()
+
+    def toggle_timer(self):
+        print("toggle_timer")
 
     def update_plot(self):
         self.time = self.time[1:]
